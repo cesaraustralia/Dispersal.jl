053b030a622af9b9bc1c12f4cbcd951323825aab
Rafael Schouten <rafaelschouten@gmail.com>
2018-06-15 20:46:18 +1000

    refactor layers

--- src/layers.jl
+++ src/layers.jl
@@ -29,17 +29,10 @@ Returns a suitability scalar from a single layer, or the product of multiple lay
 """
 suitability(layers::L, row::Int, col::Int, t) where L <: AbstractLayers =
     mapreduce(f -> suitability(getfield(layers, f), row, col, t), *, 1.0, fieldnames(L))
-
-suitability(layer::AbstractSuitabilityLayer, row::Int, col::Int, t) = layer_coord(layer.data, row, col, round(Int,t))
-suitability(layer::AbstractSuitabilitySequence, row::Int, col::Int, t) = begin
-    tf = (t + layer.timespan * 0.5) / layer.timespan # position centered in frame
-    # linear interpolation between layers in the sequence
-    tr = floor(Int, tf)
-    frac = tf - tr
-    pos1 = cyclic(tr, frames(layer.data))
-    pos2 = cyclic(tr + 1, frames(layer.data))
-    layer_coord(layer.data, row, col, pos1) * (1.0 - frac) + layer_coord(layer.data, row, col, pos2) * frac
-end
+suitability(layer::AbstractSuitabilityLayer, row::Int, col::Int, t) = 
+    layer_coord(layer.data, row, col, round(Int,t))
+suitability(layer::AbstractSuitabilitySequence, row::Int, col::Int, t) = 
+    seq_interpolate(layer, row, col, t)
 suitability(layers, row::Int, col::Int, t) = 1.0
 
 frames(data::AbstractArray{A,1}) where A <: AbstractArray = length(data)
@@ -48,6 +41,16 @@ frames(data::AbstractArray{T,3}) where T = size(data, 3)
 layer_coord(data::AbstractArray{A,1}, row::Int, col::Int, pos) where A <: AbstractArray = data[pos][row, col]
 layer_coord(data::AbstractArray{T,2}, row::Int, col::Int, pos) where T = data[row, col]
 layer_coord(data::AbstractArray{T,3}, row::Int, col::Int, pos) where T = data[row, col, pos]
+
+seq_interpolate(layer, row, col, t) = begin
+    tf = (t + layer.timespan * 0.5) / layer.timespan # position centered in frame
+    # linear interpolation between layers in the sequence
+    tr = floor(Int, tf)
+    frac = tf - tr
+    pos1 = cyclic(tr, frames(layer.data))
+    pos2 = cyclic(tr + 1, frames(layer.data))
+    layer_coord(layer.data, row, col, pos1) * (1.0 - frac) + layer_coord(layer.data, row, col, pos2) * frac
+end
 
 cyclic(t::Int, len::Int) = 
     if t > len
