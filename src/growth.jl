5ebab9c8e16b55b4bd302009d675a4657a427ad6
Rafael Schouten <rafaelschouten@gmail.com>
2018-10-31 07:10:14 -0700

    add growth models and test

--- src/growth.jl
+++ src/growth.jl
@@ -1,29 +1,29 @@
-@mix struct GrowthRate 
-    growthrate::R = 1.1 | true | (0.0, 10.0)
+@mix @columns struct GrowthRate{GR}
+    growthrate::GR = 1.1 | true | (0.0, 10.0)
 end
 
-@mix struct MinMax
-    min::M       = 0.0       | false | _
-    max::M       = 1000000.0 | false | _
+@mix @columns struct MinMax{M}
+    min::M = 0.0       | false | _
+    max::M = 1000000.0 | false | _
 end
 
-@mix struct CarryCap
-    carrycap::R = 1.1 | true | (0.0, 10.0)
+@mix @columns struct CarryCap{CC}
+    carrycap::CC = 1.1 | true | (0.0, 10.0)
 end
 
 
 " Simple fixed exponential growth rate "
-@GrowthRate struct ExponentialGrowth{R} <: AbstractModel end
+@GrowthRate struct ExponentialGrowth{} <: AbstractModel end
 
 " Simple fixed logistic growth rate "
-@GrowthRate @CarryCap struct LogisticGrowth{R} <: AbstractModel end
+@GrowthRate @CarryCap struct LogisticGrowth{} <: AbstractModel end
 
 
 " Exponential growth based on a suitability layer "
-@MinMax struct SuitabilityExponentialGrowth{M,TS} <: AbstractModel end
+@MinMax struct SuitabilityExponentialGrowth{} <: AbstractModel end
 
 " Logistic growth based on a suitability layer "
-@MinMax @CarryCap struct SuitabilityLogisticGrowth{M,TS} <: AbstractModel end
+@MinMax @CarryCap struct SuitabilityLogisticGrowth{} <: AbstractModel end
 
 SuitabilityExponentialGrowth(init::ScalableMatrix, args...) = 
     SuitabilityExponentialGrowth(init.min, init.max, args...)
@@ -34,28 +34,29 @@ SuitabilityLogisticGrowth(init::ScalableMatrix, args...) =
 " Simple suitability layer mask "
 @columns struct SuitabilityMask{ST} <: AbstractModel 
     # "Minimum habitat suitability index."
-    suitability_threshold::ST = 0.1 | true  | (0.0, 1.0)
+    suitability_threshold::ST = 0.7 | true  | (0.0, 1.0)
 end
 
 
 rule(model::ExponentialGrowth, state, args...) = state * model.growthrate
-rule(model::LogisticGrowth, state, args...) = state * model.growthrate
+rule(model::LogisticGrowth, state, args...) = state + state * model.rate * (1 - state / model.carrycap) 
 
-rule(model::SuitabilityMask, state, row, col, t, source, dest, layers, args...) = begin
+rule(model::SuitabilityMask, state, index, t, source, dest, layers, args...) = begin
     state == zero(state) && return state
-    suit = suitability(layers, (row, col), t)
+    suit = suitability(layers, index, t)
     return suit >= model.suitability_threshold ? state : zero(state)
 end
 
-rule(model::SuitabilityExponentialGrowth, state, row, col, t, source, dest, layers, args...) = begin
+rule(model::SuitabilityExponentialGrowth, state, index, t, source, dest, layers, args...) = begin
     state == zero(state) && return state
-    growthrate = suitability(layers, (row, col), t)
+    growthrate = suitability(layers, index, t)
+    max(min(state * growthrate, model.max), model.min)
+end
+
+rule(model::SuitabilityLogisticGrowth, state, index, t, source, dest, layers, args...) = begin
+    state == zero(state) && return state
+    growthrate = suitability(layers, index, t)
     state1 = state + state * growthrate * (1 - state / model.carrycap) 
     max(min(state1 , model.max), model.min)
 end
 
-rule(model::SuitabilityLogisticGrowth, state, row, col, t, source, dest, layers, args...) = begin
-    state == zero(state) && return state
-    growthrate = suitability(layers, (row, col), t)
-    max(min(state * growthrate, model.max), model.min)
-end
---
+++ test/growth.jl
@@ -0,0 +1,57 @@
+
+@testset "growth models" begin
+
+    global init =  setup([1.0 4.0 7.0;
+                          2.0 5.0 8.0;
+                          3.0 6.0 9.0])
+    
+    global output = ArrayOutput(init)
+
+
+    @testset "exponential growth" begin
+        global model = Models(ExponentialGrowth(2.0))
+        sim!(output, model, init; time=3)
+        @test output[1] == [1.0 4.0 7.0;
+                            2.0 5.0 8.0;
+                            3.0 6.0 9.0]
+        @test output[2] == [2.0  8.0 14.0;
+                            4.0 10.0 16.0;
+                            6.0 12.0 18.0]
+        @test output[3] == [ 4.0 16.0 28.0;
+                             8.0 20.0 32.0;
+                            12.0 24.0 36.0]
+    end
+
+    @testset "exponential growth with rate from suitability layer" begin
+        
+        minmaxrange = 0.0, 40.0
+
+        global init = ScalableMatrix(setup([1.0 1.0 1.0;
+                                            1.0 1.0 1.0;
+                                            1.0 1.0 1.0]), minmaxrange...)
+        global output = ArrayOutput(init)
+
+        global suit =  setup([1.0 4.0 7.0;
+                              2.0 5.0 0.5;
+                              3.0 6.0 -1.0])
+        global layers = SuitabilityLayer(suit)
+        global model = Models(SuitabilityExponentialGrowth(minmaxrange...))
+
+        sim!(output, model, init, layers; time=3)
+        @test output[1] == [1.0 1.0 1.0;
+                            1.0 1.0 1.0;
+                            1.0 1.0 1.0]
+        @test output[2] ≈  [1.0 4.0 7.0;
+                            2.0 5.0 0.5;
+                            3.0 6.0 0.0]
+        @test output[3] ≈  [1.0 16.0 40.0;
+                            4.0 25.0 0.25;
+                            9.0 36.0 0.0]
+
+        @test Cellular.normalize_frame(output[3]) == [0.025 0.4 1.0;
+                                                      0.1 0.625 0.00625;
+                                                      0.225 0.9 0.0] 
+
+    end
+
+end
