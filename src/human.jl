69b9f9b67a6dc371fcf14c13399f1a4b515f5cfc
jamesmaino <jamesmaino@gmail.com>
2018-12-20 17:47:08 +1100

    reformulate human rule

--- src/human.jl
+++ src/human.jl
@@ -2,12 +2,19 @@
 abstract type AbstractHumanDispersal <: AbstractPartialModel end
 
 # "Human dispersal model."
-@Probabilistic struct HumanDispersal{PC} <: AbstractHumanDispersal
-    precalc::PC = [] | false | _
+@limits @flattenable struct HumanDispersal{PC,H,A,D} <: AbstractHumanDispersal
+    precalc::PC              | false | _
+    human::H                 | false | _
+    par_a::A                 | true | (0.0, 1.0)
+    human_dispersal_probs::D | false | _
 end
 
-HumanDispersal(precalc::PC, cellsize::CS, prob_threshold::PT) where {PC,CS,PT} =
-    HumanDispersal{PC,CS,PT}(precalc, cellsize, prob_threshold)
+HumanDispersal(precalc, human, par_a = 1e-5) = begin
+    human_dispersal_probs = precalc_human_dispersal_probs(human, par_a)
+    HumanDispersal(precalc, human, par_a, human_dispersal_probs)
+end
+
+precalc_human_dispersal_probs(human, par_a) = 1 .- 1 ./ exp.(par_a .* human)
 
 abstract type AbstractCell end
 
@@ -23,10 +30,10 @@ mutable struct CellInterval{P,M,I} <: AbstractCell
     ind::I
 end
 
-# isless is used to teratively sort lists and search in funcitons like searchsortedfirst 
-# and partialsort! 
+# isless is used to teratively sort lists and search in funcitons like searchsortedfirst
+# and partialsort!
 # we define isless on CellMagnitude.magnitude in order to sort margnitudes in order.
-# on CellInterval it is defined on cumprop in order to randomly choose from a list 
+# on CellInterval it is defined on cumprop in order to randomly choose from a list
 # maintaining proportion when using searchsortedfirst.
 
 import Base: isless, +
@@ -48,7 +55,7 @@ precalc_human_dispersal(human::AbstractMatrix, cellsize, take, human_exponent, d
     h, w = size(human)
     indices = broadcastable_indices(Int32, human)
     dist = (distances(human) .* cellsize) .^ dist_exponent
-    dist[1] = cellsize/6 * (sqrt(2) + log(1 + sqrt(2))) # mean distance from cell centre 
+    dist[1] = cellsize/6 * (sqrt(2) + log(1 + sqrt(2))) # mean distance from cell centre
     s = similar(human)
     take = min(take, length(human))
     magnitudes = Matrix{CellMagnitude{Float32,Tuple{Int32,Int32}}}(undef, size(human)...)
@@ -84,7 +91,7 @@ end
     m.magnitude = (human[i, j] * human[ii, jj]) / (dist[abs(i - ii) + 1, abs(j - jj) + 1])
 end
 
-""" 
+"""
 Populate a matrix from a shortlist of cells from one cell in the precalculated matrix
 This lets you view the contents of a cell in an AbstractOutput display.
 
@@ -116,19 +123,28 @@ population in the source cell.
 rule!(model::AbstractHumanDispersal, data, state, index, args...) = begin
     # Ignore empty cells
     state > zero(state) || return
-    # Randomise dispersal
-    rand() < model.prob_threshold || return
 
-    # Randomly choose a cell to disperse to from the precalculated human dispersal distribution
-    shortlist = model.precalc[index...]
-    dest_id = min(length(shortlist), searchsortedfirst(shortlist, rand()))
-    dest_index = shortlist[dest_id].ind
-    # Disperse to the cell
-    update_cell!(model, data, state, dest_index)
-    state
+    dispersalprob = model.human_dispersal_probs[index...]
+    meandispersers = round(dispersalprob * state)
+
+    meandispersers = 10
+    dispersers = meandispersers # deterministic
+    # dispersers = Rand.Poisson(meandispersers) # random
+
+    for i = 1:dispersers
+        # Randomly choose a cell to disperse to from the precalculated human dispersal distribution
+        shortlist = model.precalc[index...]
+        dest_id = min(length(shortlist), searchsortedfirst(shortlist, rand()))
+        dest_index = shortlist[dest_id].ind
+        # Disperse to the cell
+        update_cell!(model, data, state, dest_index)
+    end
+    # TODO make method for boolean and float
+    data.dest[index...] -= dispersers
+    data.dest[index...]
 end
 
 update_cell!(model::AbstractHumanDispersal, data, state::AbstractFloat, dest_index) =
     data.dest[dest_index...] += oneunit(state)
-update_cell!(model::AbstractHumanDispersal, data, state::Integer, dest_index) =
+update_cell!(model::AbstractHumanDispersal, data, state::Bool, dest_index) =
     data.dest[dest_index...] = oneunit(state)
