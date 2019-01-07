96c347e8cc161101dc37a4d75b0358d6db0dc146
Rafael Schouten <rafaelschouten@gmail.com>
2019-01-08 10:58:42 +1100

    cleanup typing in allee

--- src/allee.jl
+++ src/allee.jl
@@ -1,9 +1,9 @@
 # Type declarations
 " Minimum individuals required for cell colonisation "
- @columns struct AlleeExtinction{MF} <: AbstractModel
-    minfounders::MF = 5 | true | (0.0, 100.0)
+@columns struct AlleeExtinction{MF} <: AbstractCellModel
+    minfounders::MF = 5 | true | (0, 100)
 end
 
 # Rules
 @inline rule(model::AlleeExtinction, data, state, args...) =
-    (state >= model.minfounders ? state : 0)
+    (state >= model.minfounders ? state : zero(state))
