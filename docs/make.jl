c6ffedc79e3d3d4bc75b7b5aedac89f270db2470
Rafael Schouten <rafaelschouten@gmail.com>
2019-09-09 18:27:05 +1000

    add example docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,22 @@
-using Documenter, Dispersal
+using Documenter, Dispersal, Weave, IJulia, Plots, Pkg, ColorSchemes, Colors, GeoData, JLD2
+
+example = "src/example.jmd"
+convert_doc(example, "src/example.md")
 
 makedocs(
     modules = [Dispersal],
     sitename = "Dispersal.jl",
+    pages = [
+        "Home" => "index.md",
+        "Examples" => "example.md"
+    ]
 )
 
 deploydocs(
     repo = "github.com/rafaqz/Dispersal.jl.git",
 )
+
+mkpath.(("build/pdf", "build/notebook"))
+
+weave(example, out_path="build/pdf", doctype="pandoc2pdf")
+convert_doc(example, "build/notebook/example.ipynb")
