2b4b16d3a9c9fd2297a0379052a54c573a20b929
Rafael Schouten <rafaelschouten@gmail.com>
2020-11-11 19:52:15 +1100

    remove Exact from growth model names

--- src/rules/growth.jl
+++ src/rules/growth.jl
@@ -29,7 +29,7 @@ Simple fixed exponential growth rate using exact solution.
 
 $(FIELDDOCTABLE)
 """
-struct ExactExponentialGrowth{R,W,GR,TS,S} <: GrowthRule{R,W}
+struct ExponentialGrowth{R,W,GR,TS,S} <: GrowthRule{R,W}
     "Intrinsic rate of growth per timestep"
     intrinsicrate::GR
     "Timestep used in formulation"
@@ -37,17 +37,17 @@ struct ExactExponentialGrowth{R,W,GR,TS,S} <: GrowthRule{R,W}
     "The fractional number of rule timesteps in the current simulation timestep"
     nsteps::S
 end
-ExactExponentialGrowth{R,W}(;
+ExponentialGrowth{R,W}(;
     intrinsicrate=INTRINSICRATE,
     timestep=nothing,
     nsteps=1.0,
-) where {R,W} = ExactExponentialGrowth{R,W}(intrinsicrate, timestep, nsteps)
+) where {R,W} = ExponentialGrowth{R,W}(intrinsicrate, timestep, nsteps)
 
 # DynamicGrids.jl interface
 
-precalcrule(rule::ExactExponentialGrowth, data) = precalc_timestep(rule, data)
+precalcrule(rule::ExponentialGrowth, data) = precalc_timestep(rule, data)
 
-@inline function applyrule(data, rule::ExactExponentialGrowth, population, cellindex)
+@inline function applyrule(data, rule::ExponentialGrowth, population, cellindex)
     population > zero(population) || return zero(population)
     population * exp(rule.intrinsicrate * rule.nsteps)
 end
@@ -58,7 +58,7 @@ Simple fixed logistic growth rate using exact solution
 
 $(FIELDDOCTABLE)
 """
-struct ExactLogisticGrowth{R,W,CC,GR,TS,S} <: GrowthRule{R,W}
+struct LogisticGrowth{R,W,CC,GR,TS,S} <: GrowthRule{R,W}
     "Carrying capacity for each cell. Not currently scaled by area."
     carrycap::CC
     "Intrinsic rate of growth per timestep"
@@ -68,18 +68,18 @@ struct ExactLogisticGrowth{R,W,CC,GR,TS,S} <: GrowthRule{R,W}
     "The fractional number of rule timesteps in the current simulation timestep"
     nsteps::S
 end
-ExactLogisticGrowth{R,W}(;
+LogisticGrowth{R,W}(;
     carrycap=CARRYCAP,
     intrinsicrate=INTRINSICRATE,
     timestep=nothing,
     nsteps=1.0,
-    ) where {R,W} = ExactLogisticGrowth{R,W}(carrycap, intrinsicrate, timestep, nsteps)
+    ) where {R,W} = LogisticGrowth{R,W}(carrycap, intrinsicrate, timestep, nsteps)
 
 # DynamicGrids.jl interface
 
-precalcrule(rule::ExactLogisticGrowth, data) = precalc_timestep(rule, data)
+precalcrule(rule::LogisticGrowth, data) = precalc_timestep(rule, data)
 
-@inline function applyrule(data, rule::ExactLogisticGrowth, population, cellindex)
+@inline function applyrule(data, rule::LogisticGrowth, population, cellindex)
     population > zero(population) || return zero(population)
     @fastmath (population * rule.carrycap) / (population + (rule.carrycap - population) *
                                          exp(-rule.intrinsicrate * rule.nsteps))
@@ -92,7 +92,7 @@ Exponential growth based on a growth rate data, using exact solution.
 
 $(FIELDDOCTABLE)
 """
-struct ExactExponentialGrowthMap{R,W,AK<:Val,AT,TS,S} <: GrowthMapRule{R,W}
+struct ExponentialGrowthMap{R,W,AK<:Val,AT,TS,S} <: GrowthMapRule{R,W}
     "Key for aux data"
     auxkey::AK
     "Precalculated time interpolation index for aux data"
@@ -102,21 +102,21 @@ struct ExactExponentialGrowthMap{R,W,AK<:Val,AT,TS,S} <: GrowthMapRule{R,W}
     "The fractional number of rule timesteps in the current simulation timestep"
     nsteps::S
 end
-ExactExponentialGrowthMap{R,W}(;
+ExponentialGrowthMap{R,W}(;
     auxkey,
     auxtimeindex=1,
     timestep=nothing,
     nsteps=1.0,
-) where {R,W} = ExactExponentialGrowthMap{R,W}(auxkey, auxtimeindex, timestep, nsteps)
+) where {R,W} = ExponentialGrowthMap{R,W}(auxkey, auxtimeindex, timestep, nsteps)
 
 # DynamicGrids.jl interface
 
-function precalcrule(rule::ExactExponentialGrowthMap, data)
+function precalcrule(rule::ExponentialGrowthMap, data)
     rule = precalc_timestep(rule, data)
     precalc_auxtimeindex(aux(data, rule.auxkey), rule, data)
 end
 
-@inline function applyrule(data, rule::ExactExponentialGrowthMap, population, cellindex)
+@inline function applyrule(data, rule::ExponentialGrowthMap, population, cellindex)
     population > zero(population) || return zero(population)
     intrinsicrate = auxval(data, rule.auxkey, cellindex..., rule.auxtimeindex) 
     @fastmath population * exp(intrinsicrate * rule.nsteps)
@@ -129,7 +129,7 @@ Logistic growth based on a growth rate layer, using exact solution.
 Saturation only applies with positive growth
 $(FIELDDOCTABLE)
 """
-struct ExactLogisticGrowthMap{R,W,AK<:Val,AT,CC,TS,S} <: GrowthMapRule{R,W}
+struct LogisticGrowthMap{R,W,AK<:Val,AT,CC,TS,S} <: GrowthMapRule{R,W}
     "Key for aux layer"
     auxkey::AK
     "Precalculated time interpolation index for aux data"
@@ -141,22 +141,22 @@ struct ExactLogisticGrowthMap{R,W,AK<:Val,AT,CC,TS,S} <: GrowthMapRule{R,W}
     "The fractional number of rule timesteps in the current simulation timestep"
     nsteps::S
 end
-ExactLogisticGrowthMap{R,W}(;
+LogisticGrowthMap{R,W}(;
     auxkey,
     auxtimeindex=:1,
     carrycap=CARRYCAP,
     timestep=nothing,
     nsteps=1.0,
-) where {R,W} = ExactLogisticGrowthMap{R,W}(auxkey, auxtimeindex, carrycap, timestep, nsteps)
+) where {R,W} = LogisticGrowthMap{R,W}(auxkey, auxtimeindex, carrycap, timestep, nsteps)
 
 # DynamicGrids.jl interface
 
-function precalcrule(rule::ExactLogisticGrowthMap, data)
+function precalcrule(rule::LogisticGrowthMap, data)
     rule = precalc_timestep(rule, data)
     precalc_auxtimeindex(aux(data, rule.auxkey), rule, data)
 end
 
-@inline function applyrule(data, rule::ExactLogisticGrowthMap, population, cellindex)
+@inline function applyrule(data, rule::LogisticGrowthMap, population, cellindex)
     population > zero(population) || return zero(population)
     intrinsicrate = auxval(data, rule.auxkey, cellindex..., rule.auxtimeindex) 
     if intrinsicrate > zero(intrinsicrate)
