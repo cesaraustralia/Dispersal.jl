c7f9164a71d3cbc4fdb8731d54842f855383ab6c
Rafael Schouten <rafaelschouten@gmail.com>
2021-02-10 14:19:45 +1100

    rethink nsteps for clarity and type stability

--- src/growth.jl
+++ src/growth.jl
@@ -13,9 +13,9 @@ abstract type GrowthRule{R,W} <: CellRule{R,W} end
 """
     ExponentialGrowth <: CellRule
 
-    ExponentialGrowth(; rate, timestep)
-    ExponentialGrowth{R}(; rate, timestep)
-    ExponentialGrowth{R,W}(; rate, timestep)
+    ExponentialGrowth(; rate, timestep, [nsteps_type])
+    ExponentialGrowth{R}(; rate, timestep, [nsteps_type])
+    ExponentialGrowth{R,W}(; rate, timestep, [nsteps_type])
 
 Exponential growth of population size N based on an intrinsic growth rate ``r``, using the
 exact solution between timesteps ``t`` and ``t-1``:
@@ -27,7 +27,10 @@ N_t = N_{t-1}e^{r t}
 # Keywords
 
 - `rate`: Intrinsic growth rate. May be a `Number`, an `Aux` array or another `Grid`.
-- `timestep`: Time step for the growth rate calculation, in a type compatible with the simulation `tspan`.
+- `timestep`: Time step for the growth rate calculation, in a type compatible with the 
+    simulation `tspan`.
+- `nsteps_type`: Specify the floating point type to use when `nsteps` is generated from the
+    timestep, if it is required for type-stability or performance. The default is `Float64`.
 
 Pass grid `Symbol`s to `R` or both `R` and `W` type parameters to use to specific grids.
 """
@@ -36,14 +39,15 @@ struct ExponentialGrowth{R,W,GR,TS,S} <: GrowthRule{R,W}
     timestep::TS
     nsteps::S
 end
-function ExponentialGrowth{R,W}(; 
-    rate=INTRINSICRATE_PARAM, 
-    timestep, 
+function ExponentialGrowth{R,W}(;
+    rate=INTRINSICRATE_PARAM,
+    timestep,
+    nsteps_type=Float64,
 ) where {R,W}
-    ExponentialGrowth{R,W}(rate, timestep, nothing)
+    ExponentialGrowth{R,W}(rate, timestep, zero(nsteps_type))
 end
 
-modifyrule(rule::ExponentialGrowth, data) = precalc_timestep(rule, data)
+modifyrule(rule::ExponentialGrowth, data) = precalc_nsteps(rule, data)
 
 @inline function applyrule(data, rule::ExponentialGrowth, N, I)
     N > zero(N) || return zero(N)
@@ -55,9 +59,9 @@ end
 """
     LogisticGrowth <: GrowthRule
 
-    LogisticGrowth(; rate, carrycap, timestep)
-    LogisticGrowth{R}(; rate, carrycap, timestep)
-    LogisticGrowth{R,W}(; rate, carrycap, timestep)
+    LogisticGrowth(; rate, carrycap, timestep, [nsteps_type])
+    LogisticGrowth{R}(; rate, carrycap, timestep, [nsteps_type])
+    LogisticGrowth{R,W}(; rate, carrycap, timestep, [nsteps_type])
 
 Logistic growth rate of population size ``N`` based on an intrinsic growth rate ``r`` and
 carry capacity ``K``, using the exact solution between timesteps ``t+1`` and ``t``:
@@ -73,8 +77,9 @@ These may be a `Number`, an `Aux` array or another `Grid`:
 
 - `rate`: Intrinsic growth rate.
 - `carrycap`: Carrying capacity.
-
 - `timestep`: Time step for the growth rate, in a type compatible with the simulation `tspan`.
+- `nsteps_type`: Specify the floating point type to use when `nsteps` is generated from the
+    timestep, if it is required for type-stability or performance. The default is `Float64`.
 
 Pass grid `Symbol`s to `R` or both `R` and `W` type parameters to use to specific grids.
 """
@@ -85,20 +90,21 @@ struct LogisticGrowth{R,W,GR,CC,TS,S} <: GrowthRule{R,W}
     nsteps::S
 end
 function LogisticGrowth{R,W}(;
-    rate=INTRINSICRATE_PARAM, 
-    carrycap=CARRYCAP_PARAM, 
-    timestep, 
+    rate=INTRINSICRATE_PARAM,
+    carrycap=CARRYCAP_PARAM,
+    timestep,
+    nsteps_type=Float64,
 ) where {R,W}
-    LogisticGrowth{R,W}(rate, carrycap, timestep, nothing)
+    LogisticGrowth{R,W}(rate, carrycap, timestep, zero(nsteps_type))
 end
 
-modifyrule(rule::LogisticGrowth, data) = precalc_timestep(rule, data)
+modifyrule(rule::LogisticGrowth, data) = precalc_nsteps(rule, data)
 
 @inline function applyrule(data, rule::LogisticGrowth, N, I)
     N > zero(N) || return zero(N)
 
-    rt = get(data, rule.rate, I...) * rule.nsteps 
-    k = get(data, rule.carrycap, I...) 
+    rt = get(data, rule.rate, I...) * rule.nsteps
+    k = get(data, rule.carrycap, I...)
 
     if rt > zero(rt)
         return @fastmath (N * k) / (N + (k - N) * exp(-rt))
@@ -111,7 +117,7 @@ end
     ThresholdGrowth <: CellRule
 
     ThresholdGrowth(; rate, threshold)
-    ThresholdGrowth{G}(; rate, threshold)
+    ThresholdGrowth{R}(; rate, threshold)
     ThresholdGrowth{R,W}(; rate, threshold)
 
 Simple threshold mask. Values below a certain threshold are replaced with zero.
@@ -120,8 +126,8 @@ Simple threshold mask. Values below a certain threshold are replaced with zero.
 
 These may be a `Number`, an `Aux` array or another `Grid`.
 
-- `rate`: Intrinsic growth rate. 
-- `threshold`: Minimum viability threshold below which population falls to zero.  
+- `rate`: Intrinsic growth rate.
+- `threshold`: Minimum viability threshold below which population falls to zero.
 
 Pass grid `Symbol`s to `R` or both `R` and `W` type parameters to use to specific grids.
 """
@@ -129,8 +135,8 @@ struct ThresholdGrowth{R,W,GR,Th} <: GrowthRule{R,W}
     rate::GR
     threshold::Th
 end
-function ThresholdGrowth{R,W}(; 
-    rate=INTRINSICRATE_PARAM, 
+function ThresholdGrowth{R,W}(;
+    rate=INTRINSICRATE_PARAM,
     threshold=THRESHOLD_PARAM,
 ) where {R,W}
     ThresholdGrowth{R,W}(rate, threshold)
@@ -143,7 +149,8 @@ end
     return r >= t ? N : zero(N)
 end
 
-precalc_timestep(rule, data) = precalc_timestep(rule.timestep, rule, data)
-precalc_timestep(rulestep::DatePeriod, rule, data) =
-    @set rule.nsteps = currenttimestep(data) / Millisecond(rulestep)
-precalc_timestep(rulestep, rule, data) = @set rule.nsteps = timestep(data) / rulestep
+precalc_nsteps(rule, data) = precalc_nsteps(rule.timestep, rule, data)
+precalc_nsteps(rulestep::DatePeriod, rule, data) =
+    @set rule.nsteps = typeof(rule.nsteps)(currenttimestep(data) / Millisecond(rulestep))
+precalc_nsteps(rulestep, rule, data) =
+    @set rule.nsteps = typeof(rule.nsteps)(timestep(data) / rulestep)
