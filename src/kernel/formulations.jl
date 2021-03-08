569d95284640258030a643840bc4569d57eb8bf9
vbaudrot <virgile.baudrot@posteo.net>
2021-03-08 09:32:31 +0100

    update kernel formulations

--- src/kernel/formulations.jl
+++ src/kernel/formulations.jl
@@ -27,7 +27,7 @@ y = e^{-d/λ}
 
 where λ is a shape parameter.
 """
-@Base.kwdef struct ExponentialKernel{P} <: KernelFormulation
+Base.@kwdef struct ExponentialKernel{P} <: KernelFormulation
     "Parameter for adjusting spread of dispersal propability"
     λ::P = Param(1.0, bounds=(0.0, 2.0))
 end
@@ -40,15 +40,15 @@ end
 
 Probability density function of distance ``d``.
 
-The Geometric kernel has a power-law decrease and is isotropic.
+The Geometric kernel has a power-law decrease.
 
 ```math
-y = \frac{(α+1)(α+2)}{2 π} (1+d)^α
+y = (1+d)^α (α+1)(α+2) / (2 π)
 ```
 
 where α is a shape parameter.
 """
-@Base.kwdef struct GeometricKernel{P} <: KernelFormulation
+Base.@kwdef struct GeometricKernel{P} <: KernelFormulation
     α::P = Param(1.0, bounds=(-1000.0, 1000.0))
 end
 (f::GeometricKernel)(d) = (1 + d)^f.α * ((f.α + 1)*(f.α + 2)) / (2 * π)
@@ -67,7 +67,7 @@ y = \frac{1}{π α^2} \exp\left( \frac{-d^2}{α^2}  \right)
 
 where α is a positive parameter.
 """
-@Base.kwdef struct GaussianKernel{P} <: KernelFormulation
+Base.@kwdef struct GaussianKernel{P} <: KernelFormulation
     α::P = Param(1.0, bounds=(0.0, 1000.0))
 end
 (f::GaussianKernel)(d) = 1 / (π * f.α^2) * exp(- d^2 / f.α^2)
@@ -86,7 +86,7 @@ y = \frac{β}{2 π α^2} d^{β-2} \exp\left( \frac{-d^β}{α^β}  \right)
 
 where α and β are positive parameters.
 """
-@Base.kwdef struct WeibullKernel{A,B} <: KernelFormulation
+Base.@kwdef struct WeibullKernel{A,B} <: KernelFormulation
     α::A = Param(1.0, bounds=(0.0, 1000.0))
     b::B = Param(1.0, bounds=(0.0, 1000.0))
 end
